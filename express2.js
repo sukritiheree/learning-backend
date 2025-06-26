d44dde1ebc1961a1e85cf6e9245e20e1cb4747b8
Sukriti Kanauji <sukritikanauji@gmail.com>
2025-06-26 21:17:02 +0530

    routers in express

--- express2.js
+++ express2.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const blog = require("./routes/blog");
+
 const app = express();
 const port = 3000;
 app.use(express.static("public"));
+app.use("/blog", blog);
 
 app.get("/", (req, res) => {
   console.log("hey its a get request");
@@ -14,6 +17,10 @@ app.post("/", (req, res) => {
 app.put("/", (req, res) => {
   console.log("hey its a put request");
   res.send("Hello PuT!");
+});
+app.get("/index", (req, res) => {
+  console.log("hey its INDEX");
+  res.sendFile("/templates/index.html", { root: __dirname });
 });
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
